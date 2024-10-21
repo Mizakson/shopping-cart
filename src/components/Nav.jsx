b8b1d3a527befa4c3e56c11c0127a9a6f952a189
Mizakson <maxwellizakson@gmail.com>
2024-10-21 11:00:23 -0400

    Edit testing setup, pass first Nav tests

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+        <nav>
+            <a href="">Home </a>
+            {/* <a href="">Shop </a>
+            <a href="">Cart</a> */}
+        </nav>
+    )
+}
+
+export default Nav
--- src/tests/Nav.test.jsx
+++ src/tests/Nav.test.jsx
@@ -1,4 +1,21 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react"
-import userEvent from "@testing-library/user-event";
-// component import statement here
+// import userEvent from "@testing-library/user-event";
+import Nav from "../components/Nav";
+
+describe("Nav component", () => {
+    it("renders", () => {
+        render(<Nav />)
+        screen.debug()
+    })
+})
+
+describe("bool tests", () => {
+    it("true is true", () => {
+        expect(true).toBe(true)
+    })
+
+    it("false is false", () => {
+        expect(false).toBe(false)
+    })
+})
