d605da853e8e4d18ed1e17b9655836f3dc177ca0
Mizakson <maxwellizakson@gmail.com>
2024-10-23 11:57:30 -0400

    Rename and re-ogranize component directories - Now include css module within component dir, tests still in tests dir

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-import Nav from "./components/Nav"
+import Nav from "./components/Nav/Nav"
 
 function App() {
   // const [count, setCount] = useState(0)
--- src/tests/Nav.test.jsx
+++ src/tests/Nav.test.jsx
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react"
 // import userEvent from "@testing-library/user-event";
-import Nav from "../components/Nav";
+import Nav from "../components/Nav/Nav";
 
 describe("Nav component", () => {
     it("renders", () => {
