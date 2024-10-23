9b05597ae59347a12364b7c9a5c73084a79fe563
Mizakson <maxwellizakson@gmail.com>
2024-10-23 12:08:38 -0400

    Style Nav component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,11 @@
+import styles from "./Nav.module.css"
+
 function Nav() {
     return (
         <nav data-testid="nav-bar">
-            <a href="">Home </a>
-            <a href="">Shop </a>
-            <a href="">Cart</a>
+            <a href="" className={styles.a}>Home </a>
+            <a href="" className={styles.a}>Shop </a>
+            <a href="" className={styles.a}>Cart</a>
         </nav>
     )
 }
