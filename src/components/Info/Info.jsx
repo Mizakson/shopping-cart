11a9958dd0966a4d36a0f8af7c72323101e9ad3a
Mizakson <maxwellizakson@gmail.com>
2024-10-24 12:27:17 -0400

    Add initial Info component styling

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -2,7 +2,7 @@ import styles from "./Info.module.css"
 
 function Info() {
     return (
-        <div data-testid='info'>
+        <div data-testid='info' id={styles.info}>
             <header>Header</header>
             <section>Body</section>
             <footer>Footer</footer>
