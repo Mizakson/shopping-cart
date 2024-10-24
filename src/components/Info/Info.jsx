89d83a24b45bc22c827ec059ddb2046c1b06f6f7
Mizakson <maxwellizakson@gmail.com>
2024-10-24 12:10:29 -0400

    Pass first Info component render test

---
+++ src/components/Info/Info.jsx
@@ -0,0 +1,11 @@
+import styles from "./Info.module.css"
+
+function Info() {
+    return (
+        <div data-testid='info'>
+
+        </div>
+    )
+}
+
+export default Info
--- src/tests/Info.test.jsx
+++ src/tests/Info.test.jsx
@@ -1,4 +1,12 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react"
 // import userEvent from "@testing-library/user-event";
-// component import statement here
+import Info from "../components/Info/Info"
+
+describe("Nav component", () => {
+    it("renders", () => {
+        render(<Info />)
+        const info = screen.getByTestId("info")
+        expect(info).toBeInTheDocument()
+    })
+})
