a029713508c4b9c82698a0b9f44f1c0cce8f20d5
Mizakson <maxwellizakson@gmail.com>
2024-10-28 14:01:36 -0400

    Create empty Navbar component

---
+++ src/components/navBar.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+// nav link component here
+
+function Navbar({ itemTotal }) {
+
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
+
+export default Navbar
