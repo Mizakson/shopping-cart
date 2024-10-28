309a7c5ceb122012e3f1dd0ccf60f31f5f77d092
Mizakson <maxwellizakson@gmail.com>
2024-10-28 13:40:23 -0400

    Temporary clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-// import { useState } from 'react'
-import './App.css'
+import './css/App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
-  // () => setCount((count) => count + 1)
+
 
   return (
     <>
-      <nav>
-        {/* swap to link tags */}
-        <a href="">Home</a>
-        <a href="">Shop</a>
-        <a href="">Cart() Don't Click</a>
-      </nav>
+    <h1>HI</h1>
     </>
   )
 }
