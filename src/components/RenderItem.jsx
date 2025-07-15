824ee202c0d0b27fc428d42c6243bd7b1c301b2d
Mizakson <mizakson.dev@gmail.com>
2025-07-15 15:12:21 -0400

    Edit eslint config for specific function definition

--- src/components/RenderItem.jsx
+++ src/components/RenderItem.jsx
@@ -1,5 +1,8 @@
 import PropTypes from 'prop-types';
 
+// disabling linter for id parameter, still used throughout project
+// just not in this file specifically
+// eslint-disable-next-line no-unused-vars
 function RenderItem({ id, title, image, price }) {
     return (
         <div className="top">
