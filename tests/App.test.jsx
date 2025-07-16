40dc16521eb37a8e77bf648540f30c2e8822c072
Mizakson <mizakson.dev@gmail.com>
2025-07-16 13:37:58 -0400

    Pass tests for App component

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,10 +1,27 @@
-import { describe, it, expect } from "vitest";
-import { render } from "@testing-library/react";
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "../src/App";
-import Navbar from "../src/components/navBar";
+
+// Mock the Outlet component from react-router-dom
+vi.mock('react-router-dom', async (importOriginal) => {
+
+    const actual = await importOriginal();
+    return {
+        ...actual,
+        Outlet: vi.fn(() => (
+            <div data-testid="mock-outlet">
+                Mock Outlet Content
+            </div>
+        )),
+    };
+});
 
 describe('App', () => {
+
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
 
     it('should render the App component', () => {
         render(
@@ -13,6 +30,23 @@ describe('App', () => {
             </BrowserRouter>
         );
 
-    })
+        // screen.debug() 
+        // use to see render output
 
-})
+        // assert heading and mock outlet are in the document
+        expect(screen.getByRole('heading', { name: /Main St. Shop/i })).toBeInTheDocument();
+        expect(screen.getByTestId('mock-outlet')).toBeInTheDocument();
+    });
+
+    it('should initialize itemTotal to 0 and display "Cart(0)" in the Navbar', () => {
+        render(
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        );
+
+        // Assert that there is a <p> tag with "Cart(0)" is in the document
+        expect(screen.getByText(/Cart\(0\)/i)).toBeInTheDocument();
+
+    });
+});
